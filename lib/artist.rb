6336aa22400a73ea8f59c20cf14499680143550a
Yvonne Chen <yvonnechen1991@gmail.com>
2021-01-15 00:04:40 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist
+
+    attr_reader :name
+  
+    def name=(name)
+        @name = name
+    end 
+
+end
+
+
+
+
---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+class Post
+
+    attr_reader :title, :name
+    attr_accessor :author
+
+    def title=(title)
+        @title = title
+    end 
+
+end
